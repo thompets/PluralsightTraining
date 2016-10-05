e37fd7bc4f23b8c22cbcd4ee290f85b362b43e04
Pete Thompson <peter.thompson55@gmail.com>
2016-10-04 22:48:13 -0500

    Initial commit

---
+++ PluralsightTraining/ASPNetCore/src/OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    [Route("company/[controller]/[action]")]
+    public class AboutController
+    {
+        //[Route("[action]")]
+        public string Phone()
+        {
+            return "1+555-555-5555";
+        }
+
+        //[Route("[action]")]
+        public string Address()
+        {
+            return "USA";
+        }
+    }
+}
---
+++ PluralsightTraining/ASPNetCore/src/OdeToFood/Startup.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using Microsoft.AspNetCore.Routing;
+using OdeToFood.Services;
+
+namespace OdeToFood
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(env.ContentRootPath)
+                                .AddJsonFile("appsettings.json")
+                                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton(Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
+            services.AddScoped<IRestaurantData, InMemoryRestaurantData>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory,
+            IGreeter greeter)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(new ExceptionHandlerOptions
+                {
+                    ExceptionHandler = context => context.Response.WriteAsync("oops!")
+                });
+            }
+
+            app.UseFileServer();
+
+            app.UseMvc(ConfigureRoutes);
+
+            app.Run(context => context.Response.WriteAsync("Not found"));
+
+            // order is important
+            //app.UseDefaultFiles(); // looks in web root
+            //app.UseStaticFiles();
+
+            //app.UseWelcomePage(new WelcomePageOptions
+            //{
+            //    Path = "/welcome"
+            //});           
+
+            //app.Run(async (context) =>
+            //{
+            //    var message = greeter.GetGreeting();
+            //    await context.Response.WriteAsync(message);
+            //});
+        }
+
+        private void ConfigureRoutes(IRouteBuilder routeBuilder)
+        {
+            // /Home/Index
+            routeBuilder.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
+        }
+    }
+}
